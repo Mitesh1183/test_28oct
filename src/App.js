b8b33c07323bc83f7893a425c33dfb78d811db32
Vinit Devani <devanivinitkumar@gmail.com>
2021-10-28 17:14:41 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      sss
+      <iframe
+      
+        src="http://localhost:3000/d-solo/UDdpyzz7z/prometheus-2-0-stats?orgId=2&from=1635408203306&to=1635411803306&panelId=32"
+        width="450"
+        height="200"
+        frameborder="0"
+      ></iframe>
+      <iframe 
+        src="https://mitesh1.grafana.net/d-solo/XU8HAD5Gk/usage-insights-cloud?orgId=1&refresh=30s&from=1634790419251&to=1635395219251&panelId=18"
+        width="450"
+        height="200"
+        frameborder="0"
+      ></iframe>
     </div>
   );
 }
